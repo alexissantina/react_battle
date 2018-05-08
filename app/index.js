49e87f1746a46a275eef49f747051e71aab92d20
alexissantina <alexissmj@gmail.com>
2018-05-08 13:18:50 -0700

    Base React app with UI component

---
+++ app/index.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+// state
+
+// lifecycle event
+
+// UI (the only one that is required, set with the render method)
+
+// Create a React component
+class App extends React.component {
+	render() {
+		return (
+			<div>
+				Hello World!
+			</div>
+		)
+	}
+}
